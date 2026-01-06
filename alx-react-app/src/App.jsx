3dbd45bb53fad170e26c6217a65823004b27e7eb
David Gift <giftdavid189@gmail.com>
2026-01-06 15:39:08 +0100

    update react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,21 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
-import Main from './components/MainContent'
+import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <WelcomeMessage />
     <Header />
     <Main />
-    <UserProfile name="philip" age="32" bio="best selling author of sci-fi books" />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Footer />
-      
     </>
   )
 }

d70acf1b05d6629c8a6f2af842bcadc716fc2d23
David Gift <giftdavid189@gmail.com>
2026-01-15 09:47:55 +0100

    updated styling

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return(
+        <header style={{ backgroundColor: 'navy', textColor:'white', textAlignment: 'center'}} >
+            <h1>My Favorite Cities</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile(props) {
+    return(
+    <div >
+        <h2>Name: {props.name} </h2>
+        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span> </p>
+        <p>Bio: {props.bio} </p>
+    </div>
+    );
+}
+
+export default UserProfile;

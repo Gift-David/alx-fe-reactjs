88dcec65a8075ffda24ee843c6e157aa584a4b66
David Gift <giftdavid189@gmail.com>
2026-01-16 12:44:42 +0100

    update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return(
-        <main>
+        <main style={{alignContent:'center'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );

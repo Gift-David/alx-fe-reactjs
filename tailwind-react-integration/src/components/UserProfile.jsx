4993feec342094d63784fded2e107620def6ce9c
David Gift <giftdavid189@gmail.com>
2026-02-03 10:00:59 +0100

    create responsive styles

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg my-20" >
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
-      <h1 className=" text-blue-800 my-4 text-base text-xl">John Doe</h1>
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg my-20 sm:p-4 md:p-8 max-w-xs" >
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-36 h-36 mx-auto w-24 h-24 md:h-36 w-36" />
+      <h1 className=" text-blue-800 my-4 text-base text-xl on sm:text-lg  md:text-xl sm:text-sm md:text-base">John Doe</h1>
       <p className="text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );

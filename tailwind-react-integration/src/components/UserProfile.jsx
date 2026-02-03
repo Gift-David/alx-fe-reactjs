5ecf247792d8507ec5ca6a058d92c645e9e130f4
David Gift <giftdavid189@gmail.com>
2026-02-03 09:44:55 +0100

    update styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-bray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
       <h1 className=" text-blue-800 my-4 text-base">John Doe</h1>
       <p className="text-grey-600">Developer at Example Co. Loves to write code and explore new technologies.</p>

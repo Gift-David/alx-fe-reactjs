1ed779486e4919b14a5bfebfeda041387f5d3054
David Gift <giftdavid189@gmail.com>
2026-02-03 10:23:50 +0100

    update responsive styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg my-20 sm:p-4 md:p-8 w-36 max-w-xs md:h-36 md:max-w-sm" >
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-36 sm:w-24 sm:h-24 md:w-36 md:h-36" />
-      <h1 className=" text-blue-800 my-4 text-base text-xl on sm:text-lg  md:text-xl sm:text-sm md:text-base">John Doe</h1>
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-auto rounded-lg shadow-lg my-20 sm:p-4 md:p-8 w-36 max-w-xs md:h-36 md:max-w-sm hover:shadow-xl" >
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full sm:w-36 sm:w-24 sm:h-24 md:w-36 md:h-36 transition-transform duration-300 ease-in-out hover:scale-110" />
+      <h1 className=" text-blue-800 my-4 text-base text-xl on sm:text-lg  md:text-xl sm:text-sm md:text-base hover:text-blue-500">John Doe</h1>
       <p className="text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
